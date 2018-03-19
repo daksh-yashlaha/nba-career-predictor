db9846ef18d78fb585f9ff4f1a6696f3dc9054c4
tufty-123 <dyashlaha039909@gmail.com>
2018-03-19 14:30:46 +0530

    Minor changes, added plots

--- ann.py
+++ ann.py
@@ -15,6 +15,7 @@ dataset = pd.read_csv("nba_logreg.csv", header = 0)
 X = dataset.iloc[: , 1:20].values
 y = dataset.iloc[:, 20].values
 
+#Data Pre-Processing
 #Taking care of Missing Values
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:,:])
@@ -29,6 +30,7 @@ stdscaler = StandardScaler()
 X_train = stdscaler.fit_transform(X_train)
 X_test = stdscaler.transform(X_test)
 
+#Making ANN model
 #Making first layer and first hidden layer
 classifier = Sequential()
 classifier.add(Dense(output_dim = 10, init = 'uniform', activation = 'relu', 
@@ -51,11 +53,10 @@ classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy',
                    metrics = ['accuracy'])
 
 #Fitting the ANN to the training set
-classifier.fit(X_train, y_train, batch_size = 50, epochs = 200)
+classifier.fit(X_train, y_train, batch_size = 50, epochs = 500)
 
 #Predicting the test set values
 y_pred = classifier.predict(X_test)
-y_pred2 = y_pred
 y_pred = (y_pred>0.5)
 
 #Making confusion matrix
@@ -75,6 +76,7 @@ pd.crosstab(y_test, y_pred, rownames=['True'], colnames=['Predicted'],
 
 
 #Plotting graphs
+#Plot 1
 plt.xlabel("No. of matches played")
 plt.ylabel("No. of players with career greater than 5 yrs")
 plt.title("Data Analysis")
@@ -88,4 +90,27 @@ for i in X[:,0]:
             if(((j*10)<=i)and(i<=((j+1)*10))and((y[k])==1)):
                 z[j]+=1
 plt.bar(u,z,color='blue')
+plt.show()
+
+#Plot 2
+plt.xlabel("Points Scored per Game")
+plt.ylabel("No. of players with career greater than 5 yrs")
+plt.title("Data Analysis")
+
+k=-1
+u=np.linspace(0,30,11)
+z = np.zeros(11)
+for i in X[:,2]:
+    k+=1
+    for j in range(0,10):
+            if(((j*3)<=i)and(i<=((j+1)*3))and((y[k])==1)):
+                z[j]+=1
+plt.bar(u,z,color='red')
+plt.show()
+
+#Plot 3 Confuison Matrix
+plt.title("Confusion Matrix")
+label = ['True Positive', 'True Negative', 'False Positive', 'False Negative']
+values = [cm[0,0], cm[0,1], cm[1,0], cm[1,1]]
+plt.pie(values, labels = label, shadow = True, autopct = '%.0f%%')
 plt.show()
